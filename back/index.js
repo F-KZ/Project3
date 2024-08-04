f9900b42586febfe577f99f25e2d2a149112df8f
F-KZ <supreme-soul@hotmail.fr>
2024-08-05 00:29:04 +0200

    q

--- back/index.js
+++ back/index.js
@@ -15,9 +15,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
-app.use(cors({
-    origin: 'https://project3-back.vercel.app' // Allow only this origin to access the resource
-  }));
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://project3-back.vercel.app/'
+  ];
+  
+  const corsOptions = {
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // Allow cookies and credentials to be sent
+  };
+  
+  app.use(cors(corsOptions));
+  
+  // Handle preflight requests for all routes
+  app.options('*', cors(corsOptions));
 
 // Routes
 app.use('/api/products', productRoutes)
