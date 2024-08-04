691845c2b84db2ab44813e70d1a570731a3c1d20
F-KZ <supreme-soul@hotmail.fr>
2024-08-04 03:17:59 +0200

    api

--- back/index.js
+++ back/index.js
@@ -17,26 +17,9 @@ app.use(cors());
 
 // CORS configuration
 
-const allowedOrigins = "https://project3-back.vercel.app"
-  
-  
-  const corsOptions = {
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true, // Allow cookies and credentials to be sent
-  };
-  
-  app.use(cors(corsOptions));
-  
-  // Handle preflight requests for all routes
-  app.options('*', cors(corsOptions));
+  app.use(cors({
+    origin: 'https://project3-back.vercel.app' // Allow only this origin to access the resource
+  }));
 
 // Routes
 app.use('/api/products', productRoutes)
--- front/vite.config.js
+++ front/vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://project3-back.vercel.app',
+        target: "https://project3-back.vercel.app",
         changeOrigin: true,
-        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
