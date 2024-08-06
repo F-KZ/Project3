b1b85633cb118d687e20dcb0e89196aa336e7f64
F-KZ <supreme-soul@hotmail.fr>
2024-08-06 13:03:44 +0200

    M

--- back/index.js
+++ back/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 const corsOptions = {
-    origin: '*', // Replace with your frontend's origin
+    origin: 'http://localhost:5173', // Replace with your frontend's origin
     methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
