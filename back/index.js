e0dca16c0cba1b1fb7ff58f82986d57fd4ab4a78
F-KZ <supreme-soul@hotmail.fr>
2024-08-06 20:13:40 +0200

    a

--- back/index.js
+++ back/index.js
@@ -15,15 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
-const allowedOrigin = [ 'project3-front-chi.vercel.app', 'https://project3-back.vercel.app']
-const corsOptions = {
-    origin: allowedOrigin, // Replace with your frontend's origin
-    methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  };
   
-  app.use(cors(corsOptions));
+  app.use(cors());
 
 
 // Routes
