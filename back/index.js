e7891bd30d5fe42f265415765fa3480cf54744f6
F-KZ <supreme-soul@hotmail.fr>
2024-08-06 20:50:58 +0200

    C

--- back/index.js
+++ back/index.js
@@ -15,26 +15,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
-const allowedOrigins = [
-    'https://electronic-shop-back.vercel.app',
-    'http://localhost:5173',
-    'https://project3-front-chi.vercel.app/'
-  ];
-  
-  const corsOptions = {
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true, // Allow cookies and credentials to be sent
-  };
-  
-  app.use(cors(corsOptions));
+const corsOptions = {
+    origin: 'https://project3-front-chi.vercel.app', // Replace with your frontend's origin
+    methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
   
   // Handle preflight requests for all routes
 
